1e6f0ff4f55d7b4da8b3d09dae6b6b199246419c
marvin <marvkipi@gmail.com>
2015-10-03 12:12:28 -0700

    header update

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -4,8 +4,8 @@ nflix.controller('ReviewsCtrl', function ReviewsCtrl($scope) {
   $scope.addReview = function() {
 
     $scope.reviews = [
-      { title: "Pedro in America", content: "under the table" },
-      { title: "Chico in America", content: "under the bed" }
+      { title: "Pedro in America", content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." },
+      { title: "Chico in America", content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." }
     ];
 
     $scope.reviews.push({ title: $scope.reviewTitle, content: $scope.reviewContent });
--- directives/customdirectives.js
+++ directives/customdirectives.js
@@ -5,3 +5,30 @@ nflix.directive("changeClass", function() {
     });
   }
 });
+
+
+nflix.directive("changeClass", function() {
+  return {
+    link: function (scope, element, attrs) {
+      element.bind("mouseenter", function() {
+        element.toggleClass(attrs.changeClass);
+      });
+      element.bind("mouseleave", function() {
+        element.toggleClass(attrs.changeClass);
+      });
+    }
+  };
+})
+
+nflix.directive('showonhover', function() {
+    return {
+       link : function(scope, element, attrs) {
+          element.parent().bind('mouseenter', function() {
+              element.show();
+          });
+          element.parent().bind('mouseleave', function() {
+               element.hide();
+          });
+       }
+    };
+});
