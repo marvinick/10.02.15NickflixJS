85605ecc106a73da3d505bc51b224948c891d521
marvin <marvkipi@gmail.com>
2015-10-03 14:24:21 -0700

    ng-show and ng-click works

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -6,8 +6,10 @@ nflix.controller('ReviewsCtrl', function ReviewsCtrl($scope) {
   ];
 
   $scope.addReview = function() {
-    $scope.reviews.push({ title: $scope.reviewTitle, content: $scope.reviewContent });
+    $scope.reviews.push({ title: $scope.reviewTitle, content: $scope.reviewContent, id: $scope.reviews.length + 1 });
     $scope.reviewTitle = null;
     $scope.reviewContent = null;
   };
+
+  $scope.name = {};
 });
