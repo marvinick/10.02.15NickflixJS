cf599e597381a879549dd6821041232b083db4d7
marvin <marvkipi@gmail.com>
2015-10-03 19:32:37 -0700

    nighmode daymode work - not using custom directive

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -1,8 +1,8 @@
 nflix.controller('ReviewsCtrl', function ReviewsCtrl($scope) {
 
   $scope.reviews = [
-    { title: "Pedro in America", content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." },
-    { title: "Chico in America", content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." }
+    { title: "Yeah Boi!!", content: "a phrase common among youngsters and especially young teenage girls, who often push their boyfriends to say it too. this phrase is used by people who want to sound like a ‘gangsta’ or ‘rapper’ and want to sound cool. don’t use it. usually people say “hey that phrase is mine” when no, really they heard […]" },
+    { title: "when hairy met smelly", content: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." },
   ];
 
   $scope.addReview = function() {
@@ -11,5 +11,4 @@ nflix.controller('ReviewsCtrl', function ReviewsCtrl($scope) {
     $scope.reviewContent = null;
   };
 
-  $scope.name = {};
 });
--- directives/customdirectives.js
+++ directives/customdirectives.js
@@ -1,34 +1,7 @@
-nflix.directive("changeClass", function() {
-  return function(scope, element, attrs) {
+nflix.directive("goDark", function() {
+  return function(scope, element, attrs, color) {
     element.bind("click", function() {
-      element.show(attrs.changeClass);
+      element.show = "inverted";
     });
   }
-});
-
-
-nflix.directive("changeClass", function() {
-  return {
-    link: function (scope, element, attrs) {
-      element.bind("mouseenter", function() {
-        element.toggleClass(attrs.changeClass);
-      });
-      element.bind("mouseleave", function() {
-        element.toggleClass(attrs.changeClass);
-      });
-    }
-  };
-})
-
-nflix.directive('showonhover', function() {
-    return {
-       link : function(scope, element, attrs) {
-          element.parent().bind('mouseenter', function() {
-              element.show();
-          });
-          element.parent().bind('mouseleave', function() {
-               element.hide();
-          });
-       }
-    };
 });
